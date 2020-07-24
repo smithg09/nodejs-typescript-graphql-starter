81ad262e02731fe2adbcb655e3e909a6964b7076
smithg09 <smith.gajjar@gmail.com>
2020-07-24 22:36:02 +0530

    Server Ping

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,7 +10,7 @@ export default () => {
   graphQL(app);
   agendash(app);
 
-  app.get('/server_status', (_req: Request, _res: Response) => {
+  app.get('/ping', (_req: Request, _res: Response) => {
     _res.status(200).json({
       status: 200,
       message: 'Server Connected',
