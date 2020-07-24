304de92264bb1629fec147e4fb80bc0d96359526
smithg09 <smith.gajjar@gmail.com>
2020-07-24 14:13:54 +0530

    fix:JWT Algorithm

--- src/api/middlewares/isAuth.ts
+++ src/api/middlewares/isAuth.ts
@@ -28,6 +28,9 @@ const isAuth = jwt({
   secret: config.jwtSecret, // The _secret_ to sign the JWTs
   userProperty: 'token', // Use req.token to store the JWT
   getToken: getTokenFromHeader, // How to extract the JWT from the request
+  // issuer: `localhost:3000`,
+  // restrict allowed algorithms
+  algorithms: ['HS256'],
 });
 
 export default isAuth;
