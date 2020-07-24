95882c3075dfa9ff501dc0dc2a57a2fce0106593
smithg09 <smith.gajjar@gmail.com>
2020-07-24 22:06:30 +0530

    Heroku Cleanup

--- build/loaders/events.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//Here we import all events
-require("../subscribers/user");
-//# sourceMappingURL=events.js.map
--- build/subscribers/events.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    user: {
-        signUp: 'onUserSignUp',
-        signIn: 'onUserSignIn',
-    },
-};
-//# sourceMappingURL=events.js.map
