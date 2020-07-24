7f5b26fdbb51b0bb01c3812d52e2a45a85234088
smithg09 <smith.gajjar@gmail.com>
2020-07-24 14:15:06 +0530

    wip: All Users Resolver

--- src/api/graphql/resolvers/index.ts
+++ src/api/graphql/resolvers/index.ts
@@ -3,14 +3,7 @@ import mongoose from 'mongoose';
 import { IUser } from '../../../interfaces/IUser';
 
 export const graphQlResolvers = {
-  me: async (args, req) => {
-    if (!req.isAuth) {
-      throw new Error('Unauthenticated!');
-    } else {
-      return req.currentUser;
-    }
-  },
-  allusers: async () => {
+  users: async () => {
     const UserModel = Container.get('userModel') as mongoose.Model<IUser & mongoose.Document>;
     const users = await UserModel.find({});
     return users;
