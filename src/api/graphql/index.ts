51527176dde00bc03b3b9c3a1539b1f112b6c878
smithg09 <smith.gajjar@gmail.com>
2020-09-13 10:01:07 +0530

    update: GraphQL

--- src/api/graphql/index.ts
+++ src/api/graphql/index.ts
@@ -1,25 +1,22 @@
-/* eslint-disable prettier/prettier */
 import { Router } from 'express';
-// import { Container } from 'typedi';
-// import { Logger } from 'winston';
 import middlewares from '../middlewares';
 import { graphqlHTTP } from 'express-graphql';
-import { graphQlSchema as rootSchema }  from './schema/index';
+import { graphQlSchema as rootSchema } from './schema/index';
 import { graphQlResolvers as rootResolver } from './resolvers/index';
 
-const route:Router = Router();
+const route: Router = Router();
 
 export default (app: Router) => {
-  app.use('/graphql', route);
+	app.use('/graphql', route);
 
-  route.use(middlewares.isAuth);
-  route.use(middlewares.attachCurrentUser);
-  route.use(
-    '/',
-    graphqlHTTP({
-      schema: rootSchema,
-      rootValue: rootResolver,
-      graphiql: true,
-    }),
-  );
+	route.use(middlewares.isAuth);
+	route.use(middlewares.attachCurrentUser);
+	route.use(
+		'/',
+		graphqlHTTP({
+			schema: rootSchema,
+			rootValue: rootResolver,
+			graphiql: true,
+		}),
+	);
 };
--- src/api/index.ts
+++ src/api/index.ts
@@ -5,22 +5,26 @@ import graphQL from './graphql';
 
 // guaranteed to get dependencies
 export default () => {
-  const app = Router();
-  auth(app);
-  graphQL(app);
-  agendash(app);
+	const app = Router();
+	auth(app);
+	graphQL(app);
+	agendash(app);
 
-  // Request to check if server running
-  app.get('/ping', (_req: Request, _res: Response) => {
-    _res.status(200).json({
-      status: 200,
-      message: 'Server Connected',
-    });
-  });
+	// Request to check if server running
+	app.get('/ping', (_req: Request, _res: Response) => {
+		_res.status(200).json({
+			status: 200,
+			message: 'Server Connected',
+		});
+	});
 
-  app.get('/documentation', (_req: Request, _res: Response) => {
-    _res.status(200).send('Please Visit this link for API documentation : https://documenter.getpostman.com/view/9636093/T1DpDdHt?version=latest');
-  });
+	app.get('/documentation', (_req: Request, _res: Response) => {
+		_res
+			.status(200)
+			.send(
+				'Please Visit this link for API documentation : https://documenter.getpostman.com/view/9636093/T1DpDdHt?version=latest',
+			);
+	});
 
-  return app;
+	return app;
 };
