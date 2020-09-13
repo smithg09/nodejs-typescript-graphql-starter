d31c8e2af1dd98cb3eab1bd807f0fbfd380ff91f
smithg09 <smith.gajjar@gmail.com>
2020-09-13 10:01:44 +0530

    code cleanup

--- src/subscribers/user.ts
+++ src/subscribers/user.ts
@@ -7,49 +7,52 @@ import { Logger } from 'winston';
 
 @EventSubscriber()
 export default class UserSubscriber {
-  /**
-   * A great example of an event that you want to handle
-   * save the last time a user signin, your boss will be pleased.
-   *
-   * Altough it works in this tiny toy API, please don't do this for a production product
-   * just spamming insert/update to mongo will kill it eventualy
-   *
-   * Use another approach like emit events to a queue (rabbitmq/aws sqs),
-   * then save the latest in Redis/Memcache or something similar
-   */
-  @On(events.user.signIn)
-  public onUserSignIn({ _id }: Partial<IUser>) {
-    const Logger: Logger = Container.get('logger');
+	/**
+	 * A great example of an event that you want to handle
+	 * save the last time a user signin, your boss will be pleased.
+	 *
+	 * Altough it works in this tiny toy API, please don't do this for a production product
+	 * just spamming insert/update to mongo will kill it eventualy
+	 *
+	 * Use another approach like emit events to a queue (rabbitmq/aws sqs),
+	 * then save the latest in Redis/Memcache or something similar
+	 */
+	@On(events.user.signIn)
+	public async onUserSignIn({ _id }: Partial<IUser>) {
+		const Logger: Logger = Container.get('logger');
 
-    try {
-      const UserModel = Container.get('UserModel') as mongoose.Model<IUser & mongoose.Document>;
+		try {
+			const UserModel = Container.get('userModel') as mongoose.Model<IUser & mongoose.Document>;
+			// UserModel.findById({ _id }, function(err, res) {
+			//   res.lastLogin = new Date();
+			//   res.save();
+			// });
+			await UserModel.updateOne({ _id }, { $set: { lastLogin: new Date() } });
+		} catch (e) {
+			Logger.error(`🔥 Error on event ${events.user.signIn}: %o`, e);
 
-      UserModel.update({ _id }, { $set: { lastLogin: new Date() } });
-    } catch (e) {
-      Logger.error(`🔥 Error on event ${events.user.signIn}: %o`, e);
+			// Throw the error so the process die (check src/app.ts)
+			throw e;
+		}
+	}
+	@On(events.user.signUp)
+	public onUserSignUp({ name, email, _id }: Partial<IUser>) {
+		const Logger: Logger = Container.get('logger');
 
-      // Throw the error so the process die (check src/app.ts)
-      throw e;
-    }
-  }
-  @On(events.user.signUp)
-  public onUserSignUp({ name, email, _id }: Partial<IUser>) {
-    const Logger: Logger = Container.get('logger');
+		try {
+			/**
+			 * @TODO implement this
+			 */
+			// Call the tracker tool so your investor knows that there is a new signup
+			// and leave you alone for another hour.
+			// TrackerService.track('user.signup', { email, _id })
+			// Start your email sequence or whatever
+			// MailService.startSequence('user.welcome', { email, name })
+		} catch (e) {
+			Logger.error(`🔥 Error on event ${events.user.signUp}: %o`, e);
 
-    try {
-      /**
-       * @TODO implement this
-       */
-      // Call the tracker tool so your investor knows that there is a new signup
-      // and leave you alone for another hour.
-      // TrackerService.track('user.signup', { email, _id })
-      // Start your email sequence or whatever
-      // MailService.startSequence('user.welcome', { email, name })
-    } catch (e) {
-      Logger.error(`🔥 Error on event ${events.user.signUp}: %o`, e);
-
-      // Throw the error so the process dies (check src/app.ts)
-      throw e;
-    }
-  }
+			// Throw the error so the process dies (check src/app.ts)
+			throw e;
+		}
+	}
 }
