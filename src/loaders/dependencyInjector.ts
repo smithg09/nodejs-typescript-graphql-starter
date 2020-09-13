46af12118423d2176a0cc0aefd37a580dc9d3258
smithg09 <smith.gajjar@gmail.com>
2020-09-13 10:01:53 +0530

    update: DI

--- src/loaders/dependencyInjector.ts
+++ src/loaders/dependencyInjector.ts
@@ -3,24 +3,24 @@ import LoggerInstance from './logger';
 import agendaFactory from './agenda';
 import config from '../config';
 import mailgun from 'mailgun-js';
+import Agenda from 'agenda';
 
 export default ({ mongoConnection, models }: { mongoConnection; models: { name: string; model: any }[] }) => {
-  try {
-    models.forEach(m => {
-      Container.set(m.name, m.model);
-    });
+	try {
+		models.forEach(m => {
+			Container.set(m.name, m.model);
+		});
 
-    const agendaInstance = agendaFactory({ mongoConnection });
+		const agendaInstance: Agenda = agendaFactory({ mongoConnection });
+		Container.set('agendaInstance', agendaInstance);
+		Container.set('logger', LoggerInstance);
+		Container.set('emailClient', mailgun({ apiKey: config.emails.apiKey, domain: config.emails.domain }));
 
-    Container.set('agendaInstance', agendaInstance);
-    Container.set('logger', LoggerInstance);
-    Container.set('emailClient', mailgun({ apiKey: config.emails.apiKey, domain: config.emails.domain }));
+		LoggerInstance.info('✌️ Agenda injected into container');
 
-    LoggerInstance.info('✌️ Agenda injected into container');
-
-    return { agenda: agendaInstance };
-  } catch (e) {
-    LoggerInstance.error('🔥 Error on dependency injector loader: %o', e);
-    throw e;
-  }
+		return { agenda: agendaInstance };
+	} catch (e) {
+		LoggerInstance.error('🔥 Error on dependency injector loader: %o', e);
+		throw e;
+	}
 };
